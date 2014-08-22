b32b205be215c61e5f4e6c82304241003b07b79c
MX <applimit@gmail.com>
2014-08-22 15:08:44 -0700

    MX changes
    registration page basic layout + js

---
+++ register/assets/js/register.js
@@ -0,0 +1,68 @@
+$(function() {
+
+	function validate() {
+		var last_name = $("#form_last_name").val();
+		var first_name = $("#form_first_name").val();
+		var email = $("#form_email").val();
+		var password = $("#form_password").val();
+		var password_check = $("#form_password_check").val();
+		
+		var isValidate = true;
+
+		if(last_name.length >=2) {
+			setSuccess("#control_last_name");
+		} else {
+			setError("#control_last_name", "Last name must be at least of length 2.");
+			isValidate = false;
+		}
+		
+		if(first_name.length >=2) {
+			setSuccess("#control_first_name");
+		} else {
+			setError("#control_first_name", "First name must be at least of length 2.");
+			isValidate = false;
+		}
+		
+		if(password.length >=6) {
+			setSuccess("#control_password");
+		} else {
+			setError("#control_password", "Password must be at least of length 6.");
+			isValidate = false;
+		}
+		
+		if(password == password_check) {
+			setSuccess("#control_password_check");
+		} else {
+			setError("#control_password_check", "Passwords must Match.");
+			isValidate = false;
+		}
+		
+		if (isValidate) {
+			$("#form_submit").prop('disabled', false);
+		} else {
+			$("#form_submit").prop('disabled', true);		
+		}
+		
+		return isValidate;
+		
+	}
+	
+	function setSuccess(control)  {
+		$(control).removeClass("has-error");
+		$(control).addClass("has-success");
+		$(control+">.help-block").html("");
+	}
+	
+	
+	function setError(control,error)  {
+		$(control).removeClass("has-success");
+		$(control).addClass("has-error");
+		$(control+">.help-block").html(error);
+	}
+	
+	$("#form_last_name").on('input', function() {validate();});
+	$("#form_first_name").on('input', function() {validate();});
+	$("#form_email").on('input', function() {validate();});
+	$("#form_password").on('input', function() {validate();});
+	$("#form_password_check").on('input', function() {validate();});
+});
