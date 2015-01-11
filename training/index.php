f7c9eb11d51ba4df6a5ecfecc5a79479123db664
Ming Xu <mx.enph@gmail.com>
2015-01-11 08:44:22 -0800

    Training Filter by Categories

--- training/index.php
+++ training/index.php
@@ -7,21 +7,28 @@
 ?>
 <div class="container">
 	<div class="row">
-	<div class="col-lg-8">
+	<div class="col-lg-8" id="list_container">
 	<?php
+		$category = isset($_GET['training_category']) ? intval($_GET['training_category']) : 0;
 		$limit = 5;
 		$offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 		if ($db = get_db()) {
-			$query = "SELECT * FROM `training_posts` WHERE `visible`='1' AND `is_disabled`='0';";
-			$result = $db->query($query);
+			$query = "SELECT * FROM `training_posts`";
+			$query .= " WHERE `visible`='1' AND `is_disabled`='0'";
+			if ($category!=0) {
+				$query .= " AND `training_posts`.`category`='".$category."'";
+			}
+			$result = $db->query($query.";");
 			$count = $result->num_rows;
-			$query = "SELECT * FROM `training_posts` WHERE `visible`='1' AND `is_disabled`='0' ORDER BY `publish_time` DESC LIMIT $offset,$limit;";
+			$query .= " ORDER BY `publish_time` DESC LIMIT $offset,$limit;";
+			$result = $db->query($query);
+
 			$index = 0;
 			if ($result = $db->query($query)) {
 				while ($row = $result->fetch_assoc()) {
-					$query = "SELECT `first_name`, `last_name` FROM `user_info` WHERE `id`='".$row['created_by']."';";
-					$asoc = $db->query($query)->fetch_assoc();
-					$name = $asoc['first_name'].' '.$asoc['last_name'];
+					//$query = "SELECT `first_name`, `last_name` FROM `user_info` WHERE `id`='".$row['created_by']."';";
+					//$asoc = $db->query($query)->fetch_assoc();
+					//$name = $asoc['first_name'].' '.$asoc['last_name'];
 				
 					echo '<div class="row">';
 					echo '	<div class="col-lg-12">';
@@ -41,10 +48,10 @@
 					echo '<hr><div class="row">';
 					echo '	<div class="col-sm-12">';
 					if ($count > $offset + $limit) {
-						echo '	<span style="float:right;"><a href="?offset='.($offset+5).'"><button type="button" class="btn btn-default btn-sm">Older Posts&raquo;</button></a></span>';
+						echo '	<span style="float:right;"><a href="?offset='.($offset+5).'&training_category='.($category).'"><button type="button" class="btn btn-default btn-sm">Older Posts&raquo;</button></a></span>';
 					}
 					if ($offset) {
-						echo '<a href="?offset='.($offset-5).'"><button type="button" class="btn btn-default btn-sm">&laquo; Newer Posts</button></a>';
+						echo '<a href="?offset='.($offset-5).'&training_category='.($category).'"><button type="button" class="btn btn-default btn-sm">&laquo; Newer Posts</button></a>';
 					}
 					echo '	</div>';
 					echo '</div>';
@@ -56,10 +63,31 @@
 		}
 	?>	
 	</div>
+
+	<div class="col-lg-4">
+		<br/>
+		<h3 class="text-center">Tutorial Categories</h3><br/>
+		<?php 
+			if ($db = get_db()) {
+				$query = "SELECT * FROM `training_categories`";
+				if ($result = $db->query($query)) {
+					
+					while ($row = $result->fetch_assoc()) {
+						echo '<a href="?offset='.($offset).'&training_category='.($row['training_category_id']).'"><button type="button" class="btn btn-default btn-block">'.($row['training_category_name']).'</button></a>';;
+						echo '<br/>';
+					}
+				}
+				//$result = $db->query($query);
+				//$count = $result->num_rows;
+			}
+		?>
 	</div>
-	<?php
-		print_footnote();
-	?>
+
+</div>
+
+<?php
+	print_footnote();
+?>
 
 </div><!-- /.container -->
 <?php 
