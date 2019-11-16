5e04fa2d1f5eec1ed71d57fc693cd5929d4540b2
Duc Nguyen <ngminhduc20111997@gmail.com>
2019-11-15 20:09:35 -0800

    Rotate the image if needed 2

--- manage/profile/assets/cgi/profile.php
+++ manage/profile/assets/cgi/profile.php
@@ -94,13 +94,13 @@ if ($logged_in) {
         if (!empty($exif['Orientation'])) {
             switch ($exif['Orientation']) {
                 case 3:
-                    $image = imagerotate($image, -180, 0);
+                    $image = imagerotate($image, 180, 0);
                     break;
                 case 6:
-                    $image = imagerotate($image, 90, 0);
+                    $image = imagerotate($image, -90, 0);
                     break;
                 case 8:
-                    $image = imagerotate($image, -90, 0);
+                    $image = imagerotate($image, 90, 0);
                     break;
             } 
         }
