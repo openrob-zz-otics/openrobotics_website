96039fc9293216f7ac610c64fe1fb3bb6b77ec47
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-01-07 00:04:03 -0800

    Adjust + fix bug for navigation bar

--- php_include/carousel.php
+++ php_include/carousel.php
@@ -41,6 +41,4 @@
         }
         ?>
     </div>
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 </div>
