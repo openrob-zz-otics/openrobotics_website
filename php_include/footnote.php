b7c5ba35543810a211057693f9cdd97b657b9970
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-01-15 18:45:33 -0800

    Redesign footnote

--- php_include/footnote.php
+++ php_include/footnote.php
@@ -1,23 +1,19 @@
 <hr>
-<footer>
-	<p class="pull-right">
-		<a href="https://www.linkedin.com/company/ubc-open-robotics" target="_blank">
-			<img src="/assets/images/linkedin-icon.png" width="35">
-		</a>
-		<a href="https://www.facebook.com/UBCOpenRobotics" target="_blank">
-			<img src="/assets/images/fb-icon.png" width="35">
-		</a>
-		<a href="https://twitter.com/@ubcopenrobotics" target="_blank">
-			<img src="/assets/images/twitter-icon.png" width="35">
-		</a>
-		<a href="/contact" target="_blank">
-			<img src="/assets/images/mail-icon.png" width="35">
-		</a>
-	</p>
-	<p style="padding-top:12px;">
-		&copy; 2014 UBC Open Robotics - 
-		<a href="/email/register_email"> Subscribe To Mailing List</a>
-		<!-- DONATE LINK -->		
-		- <a href="<?php echo $GLOBALS['donate_link'];?>"> Donate</a>
-	</p>
+<footer class="footnote">
+    <center class="footnote-content">
+        <p>
+            &copy; 2019 UBC Open Robotics
+        </p>
+        <p>Hennings Bldg, Vancouver, BC V6T 1Z1</p>
+        <br />
+        <a href="https://www.linkedin.com/company/ubc-open-robotics" target="_blank">
+            <img src="/assets/images/linkedin-icon.png" width="35">
+        </a>
+        <a href="https://www.facebook.com/UBCOpenRobotics" target="_blank">
+            <img src="/assets/images/fb-icon.png" width="35">
+        </a>
+        <a href="https://twitter.com/@ubcopenrobotics" target="_blank">
+            <img src="/assets/images/twitter-icon.png" width="35">
+        </a>
+    </center>
 </footer>
