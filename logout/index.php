1b566311ae21ddf8acdd5a057979e014a7ed3854
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-01-17 23:21:11 -0800

    Fix margin-left

--- logout/index.php
+++ logout/index.php
@@ -1,30 +1,36 @@
 <?php
-	//include our library and start drawing the page
-	require_once("../php_include/functions.php");
-	$page_name = "logout";
-	print_header($page_name, false);
-	
-	//logout
-	session_destroy();
-	$GLOBALS['logged_in'] = false;
-	$GLOBALS['user_email'] = "";
-	
-	print_navbar();
+//include our library and start drawing the page
+require_once("../php_include/functions.php");
+$page_name = "logout";
+print_header($page_name, false);
+
+//logout
+session_destroy();
+$GLOBALS['logged_in'] = false;
+$GLOBALS['user_email'] = "";
+
+print_navbar();
 
 ?>
 
 <div class="container">
 
-	<h2>Logout Success</h2>
-	
-	<?php
-		print_footnote();
-	?>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <h2>Logout Success</h2>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+
+    <?php
+    print_footnote();
+    ?>
 
 </div><!-- /.container -->
 
 
-<?php 
-	//print the footer	
-	print_footer();
+<?php
+//print the footer	
+print_footer();
 ?>
--- recruitment/index.php
+++ recruitment/index.php
@@ -18,32 +18,34 @@ print_navbar();
     <br />
     <section class="services">
         <div class="row">
-            <div class="col-md-4">
-                <div class="service-item first-service">
-                    <div class="service-icon"></div>
-                    <h2>Beginner</h2>
-                    <p>You're probably a first or second year student with a passion for technology and a desire to develop your hands on skills. You're ready to put some time into it, but you don't know much about robotics... yet. What we're looking for: Enthusiasm, some basic hands on skills, demonstrated commitment. Don't forget you'll only get out what you put in.</p>
-                    <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Beginner" role="button">Register &raquo;</a>
+            <div class="mtt-content">
+                <div class="col-md-4">
+                    <div class="service-item first-service">
+                        <div class="service-icon"></div>
+                        <h2>Beginner</h2>
+                        <p>You're probably a first or second year student with a passion for technology and a desire to develop your hands on skills. You're ready to put some time into it, but you don't know much about robotics... yet. What we're looking for: Enthusiasm, some basic hands on skills, demonstrated commitment. Don't forget you'll only get out what you put in.</p>
+                        <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Beginner" role="button">Register &raquo;</a>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-sm-4">
-                <div class="service-item second-service">
-                    <div class="service-icon"></div>
-                    <h2>Intermediate</h2>
-                    <p>You're probably a second or third year student who has done some mechanical design, electrical design, or coding already. You're getting more comfortable with your skills but you want to take them to the next level.
-                        This is your chance. We will pair you with an expert mentor, provide the resources and guidance you need, and give you a place to show off your project. You'll be expected to put in more time into CAD/coding/fabrication than your mentor, but hey, it's more fun than studying and it's what gets you the good jobs.</p>
-                    <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Intermediate" role="button">Register &raquo;</a>
-                </div>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-sm-4">
-                <div class="service-item third-service">
-                    <div class="service-icon"></div>
-                    <h2>Advanced</h2>
-                    <p>You're probably a fourth year to graduate student who has significant experience in mechanical design, control, software, or robotics. We know how busy you are and we appreciate whatever you can put into the team. You are the mentors, and the most important contributors to our training program. </p>
-                    <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Advanced" role="button">Register &raquo;</a>
-                </div>
-            </div><!-- /.col-lg-4 -->
+                <div class="col-sm-4">
+                    <div class="service-item second-service">
+                        <div class="service-icon"></div>
+                        <h2>Intermediate</h2>
+                        <p>You're probably a second or third year student who has done some mechanical design, electrical design, or coding already. You're getting more comfortable with your skills but you want to take them to the next level.
+                            This is your chance. We will pair you with an expert mentor, provide the resources and guidance you need, and give you a place to show off your project. You'll be expected to put in more time into CAD/coding/fabrication than your mentor, but hey, it's more fun than studying and it's what gets you the good jobs.</p>
+                        <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Intermediate" role="button">Register &raquo;</a>
+                    </div>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-sm-4">
+                    <div class="service-item third-service">
+                        <div class="service-icon"></div>
+                        <h2>Advanced</h2>
+                        <p>You're probably a fourth year to graduate student who has significant experience in mechanical design, control, software, or robotics. We know how busy you are and we appreciate whatever you can put into the team. You are the mentors, and the most important contributors to our training program. </p>
+                        <a class="btn btn-default btn-custom" href="/recruitment/recruit_form?level=Advanced" role="button">Register &raquo;</a>
+                    </div>
+                </div><!-- /.col-lg-4 -->
+            </div>
         </div><!-- /.row -->
     </section>
 
