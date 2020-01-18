b690037d25503558de5292ab04bb1967834c2cc8
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-01-17 23:27:43 -0800

    Minor style adjustment

--- login/login_success.php
+++ login/login_success.php
@@ -8,11 +8,10 @@ print_navbar();
 
 <div class="container">
     <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-1"></div>
+        <div class="col-md-11">
             <h2>Login Success</h2>
         </div>
-        <div class="col-md-2"></div>
     </div>
 
     <?php
--- logout/index.php
+++ logout/index.php
@@ -16,11 +16,10 @@ print_navbar();
 <div class="container">
 
     <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-1"></div>
+        <div class="col-md-11">
             <h2>Logout Success</h2>
         </div>
-        <div class="col-md-2"></div>
     </div>
 
     <?php
