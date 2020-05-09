142d7d339f20b2fb2c628eab8787491402e19378
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-05-09 12:02:11 -0700

    Adjust the position of the content 3

--- manage/projects/index.php
+++ manage/projects/index.php
@@ -16,32 +16,35 @@
 	if (!canAddProjects() && !canManageAllProjects()) {
 		echo '<div class="row"><div class="mtt-content"><h3>You do not have permission to be here.</h3></div></div>';
 	} else {
-		echo '<div class="row">
-		<div class="mtt-content">
-			<a href="project?id=0"><button class="btn btn-default">New Project</button></a>
-		</div>
-		<div class="col-md-8">
-		<p>Edit Existing Projects</p>
-		<ul>';
-		
-			if ($db = get_db()) {
-				$query;
-				if (canManageAllProjects()) {
-					$query = "SELECT * FROM `projects` WHERE `is_disabled`='0';";
-				} else {
-					$query = "SELECT * FROM `projects` WHERE `is_disabled`='0' AND (`created_by`='$user_id' OR `id` IN (SELECT `project_id` FROM `project_contributors` WHERE `user_id`='$user_id'));";
-				}	
-				if ($result = $db->query($query)) {
-					while ($row = $result->fetch_assoc()) {
-						echo "<li><a href='project?id=".$row['id']."'>".$row['name'].' - '.$row['start_time'].' - '.$row['finish_time']."</a></li>";
-					}	
-				}
-				$db->close();
-			}
-		
-		echo '</ul>
-		</div>
-	</div>';
+		echo '
+		<div class="row">
+			<div class="mtt-content">
+				<div class="col-md-4">
+					<a href="project?id=0"><button class="btn btn-default">New Project</button></a>
+				</div>
+				<div class="col-md-8">
+					<p>Edit Existing Projects</p>
+					<ul>';
+			
+						if ($db = get_db()) {
+							$query;
+							if (canManageAllProjects()) {
+								$query = "SELECT * FROM `projects` WHERE `is_disabled`='0';";
+							} else {
+								$query = "SELECT * FROM `projects` WHERE `is_disabled`='0' AND (`created_by`='$user_id' OR `id` IN (SELECT `project_id` FROM `project_contributors` WHERE `user_id`='$user_id'));";
+							}	
+							if ($result = $db->query($query)) {
+								while ($row = $result->fetch_assoc()) {
+									echo "<li><a href='project?id=".$row['id']."'>".$row['name'].' - '.$row['start_time'].' - '.$row['finish_time']."</a></li>";
+								}	
+							}
+							$db->close();
+						}
+					
+					echo '</ul>
+				</div>
+			</div>
+		</div>';
 	}
 		print_footnote();
 	?>
