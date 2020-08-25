bc09f41c6b8b72f5a63a3d350c394c99ca09d915
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-08-24 17:52:19 -0700

    Adjust recruitment text

--- recruitment/index.php
+++ recruitment/index.php
@@ -9,8 +9,7 @@ print_navbar();
 <div class="container">
     <div class="mtt-content">
         <h2>So we've got your interest, what's next?<span class="text-muted"></h2>
-        <p>Although we plan to have a truly open team which can accept any motivated individual, our training program
-            is not complete and the team is still small. Right now we are mostly looking for technically experienced individuals who can help write the training program, improve the website,
+        <p>Right now we are looking for technically experienced individuals who can help write the training program, improve the website,
             and complete the first iteration of our full-size robotics platform. If that's not you don't worry, just follow us on social media, and try and do a couple small hands on projects.
             We'll post suggestions soon!</span></p>
     </div>
