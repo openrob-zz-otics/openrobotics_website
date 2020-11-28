d8fb2628c9a1c269579272787cbe8150559ed267
Amir Ali Barkam <amirbarkam6@gmail.com>
2020-11-28 15:02:04 -0800

    Make date on recruitment page text bold (#130)

--- recruitment/index.php
+++ recruitment/index.php
@@ -10,7 +10,7 @@ print_navbar();
     <div class="mtt-content">
         <h2>So we've got your interest, what's next?</h2>
         <p>
-            Our Fall Recruitment Cycle is now closed, but stay tuned! Our next recruitment cycle will open on January 1st, 2021. 
+            Our Fall Recruitment Cycle is now closed, but stay tuned! Our next recruitment cycle will open on <b>January 1st, 2021</b>. 
             In the mean time follow us on <a href="https://www.facebook.com/UBCOpenRobotics/" target="_blank">Facebook</a>, 
             <a href="https://www.instagram.com/ubcopenrobotics/" target="_blank">Instagram</a>, or <a href="https://twitter.com/ubcopenrobotics" target="_blank">Twitter</a> 
             or email us at <a href="mailto:intelligence@openrobotics.ca">intelligence@openrobotics.ca</a> if you have any questions. 
