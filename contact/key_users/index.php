48177a9ee321007bab2025f8d92ccb3dce093219
Amir Barkam <amiralib>
2021-06-26 12:18:40 -0700

    Add admin mentor section

--- contact/key_users/index.php
+++ contact/key_users/index.php
@@ -121,6 +121,9 @@ print_navbar();
                                         else if ($row['id'] == 64) {
                                             echo '<p>Safety Officer</p> </a> </div> </div>';
                                         }
+										else if ($row['id'] == 24) {
+											echo '<p>Mentor</p> </a> </div> </div>';
+										} 
                                     }
                                     else if ($teams[$i] === 'First Year Mentorship') {
                                         echo '<p>Mentee</p> </a> </div> </div> ';
